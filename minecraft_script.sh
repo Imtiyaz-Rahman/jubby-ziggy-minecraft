9f8726c2133cc3cb92ed10c5d2a2ac96967de5dc
Imtiyaz <130472000+Imtiyaz-Rahman@users.noreply.github.com>
2025-04-12 21:20:04 +0100

    Added shell script

---
+++ minecraft_script.sh
@@ -0,0 +1,29 @@
+# !/bin/bash
+
+echo "Hello, Pain!"
+echo "Your information is now being stolen"
+
+# make new directory and move contents
+mkdir server
+mkdir vm-setup
+mv minecraft_script.sh vm-setup
+
+# update system
+sudo apt update
+sudo apt upgrade
+
+# insert Readme for jdk
+printf '# Install your specific commands for your jdk version' >> vm-setup/jdklist.md
+printf '# apt install openjdk-1-jdk' >> vm-setup/jdklist.md
+printf '# (Change the number depending on java jdk version you want to play on)' >> vm-setup/jdklist.md
+
+# setup firewalld
+yes |sudo apt install firewalld
+
+firewall-cmd --permanent --zone=public --add-port=25565/tcp 
+firewall-cmd --permanent --zone=public --add-port=25565/udp 
+firewall-cmd --reload
+
+ 
+
+echo "everything should be compelete now good luck"
